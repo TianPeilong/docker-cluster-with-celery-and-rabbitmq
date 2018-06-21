28eefa05195fa57592a3199e0d34d7c8089dc0ce
tianpl <tianpl@c-bim.net>
2018-06-21 17:57:36 +0800

    '默认'

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 from celery import Celery
-app = Celery('test_celery',broker='redis://redis_db:6379/0',backend='redis://redis_db:6379/0',include=['test_celery.tasks'])
+app = Celery('test_celery',broker='redis://172.168.1.102:6370/0',backend='redis://172.168.1.102:6370/0',include=['test_celery.tasks'])
+app.config_from_object('celeryconfig')
 
