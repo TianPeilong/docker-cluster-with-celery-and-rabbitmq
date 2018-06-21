5618c02cb49412cf8ce8cf846d8f9960ff4f7881
tianpl <tianpl@c-bim.net>
2018-06-21 18:21:24 +0800

    '默认'

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from celery import Celery
+import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryconfig') 
 app = Celery('test_celery',broker='redis://172.168.1.102:6370/0',backend='redis://172.168.1.102:6370/0',include=['test_celery.tasks'])
 app.config_from_object(celeryconfig)
