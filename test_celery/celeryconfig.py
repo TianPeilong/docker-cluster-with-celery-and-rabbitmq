1746028d5939ae7f8406ebf1a9d9dc539b2759ad
tianpl <tianpl@c-bim.net>
2018-06-22 15:10:34 +0800

    '默认'

--- test_celery/celeryconfig.py
+++ test_celery/celeryconfig.py
@@ -1,5 +1,6 @@
 from kombu.serialization import registry
 
 CELERY_ACCEPT_CONTENT = ['json','application/text']
+CELERYD_POOL_RESTARTS = True  # Required for /worker/pool/restart API
 registry.enable('json')
 registry.enable('application/text')
