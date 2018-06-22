d1b728de359dd88301d19a986420a8f0fdfd636b
tianpl <tianpl@c-bim.net>
2018-06-22 14:37:17 +0800

    '默认'

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import
 from celery import Celery
 import os
 
-CELERY_BROKER = 'redis://172.168.1.102:6370/0'
+CELERY_BROKER = 'redis://redis_db:6379/0'
 if 'CELERY_BROKER' in os.environ:
     CELERY_BROKER = os.environ['CELERY_BROKER']
 
-CELERY_BACKEND = 'redis://172.168.1.102:6370/0'
+CELERY_BACKEND = 'redis://redis_db:6379/0'
 if 'CELERY_BACKEND' in os.environ:
     CELERY_BACKEND = os.environ['CELERY_BACKEND']
 
